b12a1edce9a1d3016659c5eb5107c3e4f301f4c5
Jon Balon <development@jonbalon.com>
2022-06-07 16:34:47 -0400

    `Book` struct

---
+++ go-bookstore/pkg/models/book.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"go-bookstore/pkg/config"
+
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+// Initialize database
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
