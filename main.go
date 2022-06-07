98a1b61d8e207fc2e3d390b481dbccce1d111c54
Jon Balon <development@jonbalon.com>
2022-06-06 21:50:28 -0400

    Placeholder Handle Function for the `PUT /movies/{id}` Route

--- main.go
+++ main.go
@@ -32,10 +32,15 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	// To be implemented.
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// To be implemented.
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 }
