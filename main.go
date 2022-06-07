4837fe9be87bccd072d94b8d45bbd9508adfcfb8
Jon Balon <development@jonbalon.com>
2022-06-06 22:16:56 -0400

    Implementation `getMovie()` Handle Function

--- main.go
+++ main.go
@@ -29,7 +29,16 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
-	// To be implemented.
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
