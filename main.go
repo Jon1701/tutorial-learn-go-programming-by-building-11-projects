51cf9d2b783f6b1919876765133eea17aa42713d
Jon Balon <development@jonbalon.com>
2022-06-06 22:38:26 -0400

    Implementation `updateMovie()` Handle Function

--- main.go
+++ main.go
@@ -58,7 +58,24 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
-	// To be implemented.
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// Delete movie
+			movies = append(movies[:index], movies[index+1:]...)
+
+			// Create new movie
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
